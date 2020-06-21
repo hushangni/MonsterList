fd7f442c1360c780868258ed58e85a2e53b9c097
Shangni Hu <shu@katalystadvantage.com>
2020-06-21 13:16:11 -0400

    Updates

---
+++ precache-manifest.10563c9835ed2cc4cc16bc0e68b192b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391ca6bdbaa9b377727d161e689b9c94",
+    "url": "/MonsterList/index.html"
+  },
+  {
+    "revision": "eeccde7912e9749b41ed",
+    "url": "/MonsterList/static/css/main.67eabd88.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MonsterList/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterList/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeccde7912e9749b41ed",
+    "url": "/MonsterList/static/js/main.32a55654.chunk.js"
+  },
+  {
+    "revision": "84e65b8aad7c37656c42",
+    "url": "/MonsterList/static/js/runtime-main.b9a431fc.js"
+  }
+]);
